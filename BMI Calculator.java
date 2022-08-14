db48fd8dcc89d636ba1b4a58a7fc0a20aed39a78
Mahika Agrawal <80638498+mahikkaaa@users.noreply.github.com>
2022-08-14 14:50:05 +0530

    Update BMI Calculator.java

--- BMI Calculator.java
+++ BMI Calculator.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 public class Main{
     public static void main (String[] args){
-        System.out.println("hello nikhil");
+        System.out.println("Welcome to BMI Calculator");
         Scanner input = new Scanner(System.in);
+        System.out.println("enter your name: ");
         String name = input.next();
-        System.out.println("enter your name"+ name);
         System.out.println("enter your height in m : ");
         float height= input.nextFloat();
         System.out.println("enter your weight in kgs : ");
         float weight= input.nextFloat();
         System.out.println("enter number of days you workout : ");
-        float days= input.nextInt();
+        float days= input.nextFloat();
         float BMI= weight/(height*height);
-        System.out.println("HEY"+ name + "your height is" + height + "and your weight is" + weight + "so your BMI is " + BMI);
+        System.out.println("Hey"+ name + "your height is" + height + "and your weight is" + weight + "so your BMI is " + BMI);
     }
 }
