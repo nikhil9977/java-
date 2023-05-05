0c2efda16fcca504105540f5b834ccf9024f7e00
nikhil <nikhil8948947219@gmail.com>
2023-05-06 02:18:14 +0530

    s

--- BMI Calculator.java
+++ BMI Calculator.java
@@ -15,9 +15,10 @@ public class Main{
         System.out.println("Hey"+ name + "your height is" + height + "and your weight is" + weight + "so your BMI is " + BMI);
    
 
+     
         
 
-        
+
 
 
     }
